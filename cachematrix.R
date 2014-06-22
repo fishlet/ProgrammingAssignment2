123ae4036884765f5e9feca740a950f4c75989ee
fishlet <fishlet0528@gmail.com>
2014-06-22 14:29:03 +0800

    [R Programming] implement functions makeCacheMatrix and cacheSolve for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this file contains two functions: makeCacheMatrix & cacheSolve
+## see respective comments for more details
 
-## Write a short comment describing this function
-
+## compute and return a special matrix containing getters and setters 
+## for both the input matrix and its cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function() x
+  setMatrixInverse <- function(k) inverse <<- k
+  getMatrixInverse <- function() inverse
+  matrix(data = c(getMatrix, setMatrix, getMatrixInverse, setMatrixInverse), 
+         nrow = 2, ncol = 2, 
+         byrow = TRUE, 
+         dimnames = list( c("matrix", "inverse"), c("getter", "setter") )
+  )
 }
 
 
-## Write a short comment describing this function
-
+## takes the output of the function makeCacheMatrix and:
+## check if the cached matrix inverse exists
+##    if yes: print log and return the cached matrix inverse
+##    if not: get the original matrix data, compute its inverse, then cache and return the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x[["inverse", "getter"]]()
+  if(!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  data <- x[["matrix", "getter"]]()
+  inverse <- solve(data)
+  x[["inverse", "setter"]](inverse)
+  inverse
 }
